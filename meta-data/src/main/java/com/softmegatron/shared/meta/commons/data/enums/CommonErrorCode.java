34942b8140f9a0658c399b2467bbcf34ac958f53
Wei YAN <wei.yan@softmegatron.com>
2026-02-13 23:00:01 +0800

    ``` refactor: rename meta-commons modules to meta modules
    - Rename artifactId from meta-commons-core to meta-core in pom.xml
    - Rename artifactId from meta-commons-data to meta-data in pom.xml
    - Rename artifactId from meta-commons-data-ext to meta-data-ext in pom.xml
    - Update dependency references from meta-commons-data to meta-data
    - Rename all package paths from meta-commons-* to meta-*
    - Make enum fields final and remove setter methods in CommonErrorCode
    - Make enum fields final and remove setter methods in MessageCode
    ```

---
+++ meta-data/src/main/java/com/softmegatron/shared/meta/commons/data/enums/CommonErrorCode.java
@@ -0,0 +1,31 @@
+package com.softmegatron.shared.meta.commons.data.enums;
+
+import com.softmegatron.shared.meta.commons.data.base.ReturnCode;
+
+/**
+ * CommonErrorCode
+ *
+ * @author <a href="mailto:wei.yan@softmegatron.com">wei.yan</a>
+ * @version 1.0.0
+ * @since 2019-05-12 00:31
+ */
+public enum CommonErrorCode implements ReturnCode {
+    ;
+
+    private final String code;
+
+    private final String desc;
+
+    CommonErrorCode(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
